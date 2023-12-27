8add70110693c9e76262d84d99534109f146344d
faheem303 <faheemrehmat360@gmail.com>
2023-12-28 03:56:06 +0500

    final changes

--- src/Page/Home/LicensedCardSection.jsx
+++ src/Page/Home/LicensedCardSection.jsx
@@ -22,7 +22,7 @@ const LicensedCardSection = () => {
                         <p className=" font-[inter] text-[13.5px] font-[400] leading-[20.064px] text-white">{t('LicensedCard.card1Para')}</p>
                     </div>
                     <div className="">
-                        <img className=" w-[163px] 2xl:h-[120px] xl:h-[120px] lg:h-[110px] md:h-[auto] sm:h-[auto] h-[auto]" src={licimg1} alt="" />
+                        <img className=" w-[163px] 2xl:h-[120px] xl:h-[auto] lg:h-[110px] md:h-[auto] sm:h-[auto] h-[auto]" src={licimg1} alt="" />
                     </div>
                 </div>
                 <div className=" rounded-[32px] flex justify-between pl-[32px]  py-[38px] border LicensedCard border-[#FFF89C] h-[auto] 2xl:h-[215px] xl:h-[220px] max-w-[432px] w-[100%]">
@@ -31,7 +31,7 @@ const LicensedCardSection = () => {
                         <p className=" font-[inter] text-[14px] font-[400] leading-[20.064px] text-white">{t('LicensedCard.card2Para')}</p>
                     </div>
                     <div className="">
-                        <img className=" w-[163px] 2xl:h-[120px] xl:h-[120px] lg:h-[auto] md:h-[auto] sm:h-[auto] h-[auto]" src={licimg3} alt="" />
+                        <img className=" w-[163px] 2xl:h-[120px] xl:h-[auto] lg:h-[auto] md:h-[auto] sm:h-[auto] h-[auto]" src={licimg3} alt="" />
                     </div>
                 </div>
                 <div className=" rounded-[32px] flex justify-between pl-[32px]  py-[38px] border LicensedCard border-[#FFF89C] h-[auto] 2xl:h-[215px] xl:h-[220px] max-w-[432px] w-[100%]">
@@ -40,7 +40,7 @@ const LicensedCardSection = () => {
                         <p className=" font-[inter] text-[14px] font-[400] leading-[20.064px] text-white">{t('LicensedCard.card3Para')}</p>
                     </div>
                     <div className="">
-                        <img className=" w-[163px] 2xl:h-[120px] xl:h-[120px] lg:h-[110px] md:h-[auto] sm:h-[auto] h-[auto]" src={licimg2} alt="" />
+                        <img className=" w-[163px] 2xl:h-[120px] xl:h-[auto] lg:h-[110px] md:h-[auto] sm:h-[auto] h-[auto]" src={licimg2} alt="" />
                     </div>
                 </div>
 

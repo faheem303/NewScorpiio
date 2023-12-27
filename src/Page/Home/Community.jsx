4aa2b9e69b64b51e69d852aa0a1d8b84eb4e920b
faheem303 <faheemrehmat360@gmail.com>
2023-12-28 02:07:18 +0500

    final changes

--- src/Page/Home/Community.jsx
+++ src/Page/Home/Community.jsx
@@ -28,10 +28,10 @@ const CommunitySection = () => {
               <div className="commob 2xl:w-[31%] xl:w-[31%] lg:w-[31%] md:w-[100%] sm:w-[100%] w-[100%]  ">
                   <div className="commbgmob flex flex-col justify-center  px-[32px] py-[32px] space-y-4">
                     <img className="h-[25px] w-[25px]" src={colan} alt="" />
-                    <p className="text-[16px] 2xl:h-[190px] xl:h-[200px] lg:h-[185px] md:h-[120px] sm:h-[120px] h-[195px] text-[white] opacity-80"> {t('CommunitySection.cardPara1')}</p>
+                    <p className="text-[16px] 2xl:h-[180px] xl:h-[200px] lg:h-[280px] md:h-[120px] sm:h-[120px] h-[195px] text-[white] opacity-80"> {t('CommunitySection.cardPara1')}</p>
                     <div className="flex relative justify-center flex-col items-center">
                         <img className="border-2 rounded-full border-[#FFD770] h-[53px] w-[52px]" src={smit} alt="" />
-                        <h4 className="uppercase gradientText text-[15px] font-[900] font-[Inter]">{t('CommunitySection.cardTitle1')}</h4>
+                        <h4 className="uppercase pt-2  gradientText text-[15px] font-[900] font-[Inter]">{t('CommunitySection.cardTitle1')}</h4>
                         <a href="https://t.me/scorpioncasino_official/46092" target="_blank">
                         <p className="text-[#B6BCCC] text-[13px] font-[400] font-[Inter] underline cursor-pointer ">{t('CommunitySection.CardSubTitle')}</p>
                         </a>
@@ -42,10 +42,10 @@ const CommunitySection = () => {
                 <div className="commob 2xl:w-[31%] xl:w-[31%] lg:w-[31%] md:w-[100%] sm:w-[100%] w-[100%]  ">
                   <div className="commbgmob flex flex-col justify-center  px-[32px] py-[32px] space-y-4">
                     <img className="h-[25px] w-[25px]" src={colan} alt="" />
-                    <p className="text-[16px] 2xl:h-[190px] xl:h-[200px] lg:h-[185px] md:h-[120px] sm:h-[120px] h-[195px] text-[white] opacity-80">{t('CommunitySection.cardPara2')} </p>
+                    <p className="text-[16px] 2xl:h-[180px] xl:h-[200px] lg:h-[280px] md:h-[120px] sm:h-[120px] h-[195px] text-[white] opacity-80">{t('CommunitySection.cardPara2')} </p>
                     <div className="flex relative justify-center flex-col items-center">
                         <img className="border-2 rounded-full border-[#FFD770] h-[53px] w-[52px]" src={smit2} alt="" />
-                        <h4 className="uppercase gradientText text-[15px] font-[900] font-[Inter]"> {t('CommunitySection.cardTitle2')}</h4>
+                        <h4 className="uppercase pt-2  gradientText text-[15px] font-[900] font-[Inter]"> {t('CommunitySection.cardTitle2')}</h4>
                         <a href="https://t.me/scorpioncasino_official/46235">
                         <p className="text-[#B6BCCC] text-[13px] font-[400] font-[Inter] underline cursor-pointer ">{t('CommunitySection.CardSubTitle')}</p>
                         </a>
@@ -57,10 +57,10 @@ const CommunitySection = () => {
                 <div className="commob 2xl:w-[31%] xl:w-[31%] lg:w-[31%] md:w-[100%] sm:w-[100%] w-[100%]  ">
                   <div className="commbgmob flex flex-col justify-center  px-[32px] py-[32px] space-y-4">
                     <img className="h-[25px] w-[25px]" src={colan} alt="" />
-                    <p className="text-[16px] 2xl:h-[200px] xl:h-[200px] lg:h-[185px] md:h-[120px] sm:h-[120px] h-[195px] text-[white] opacity-80">{t('CommunitySection.cardPara3')} </p>
+                    <p className="text-[16px] 2xl:h-[180px] xl:h-[200px] lg:h-[280px] md:h-[120px] sm:h-[120px] h-[195px] text-[white] opacity-80">{t('CommunitySection.cardPara3')} </p>
                     <div className="flex relative justify-center flex-col items-center">
                         <img className="border-2 rounded-full border-[#FFD770] h-[53px] w-[52px]" src={smit3} alt="" />
-                        <h4 className="uppercase gradientText text-[15px] font-[900] font-[Inter]"> {t('CommunitySection.cardTitle3')}</h4>
+                        <h4 className="uppercase pt-2  gradientText text-[15px] font-[900] font-[Inter]"> {t('CommunitySection.cardTitle3')}</h4>
                         <a href="https://t.me/scorpioncasino_official/46264
 ">
                         <p className="text-[#B6BCCC] text-[13px] font-[400] font-[Inter] underline cursor-pointer ">{t('CommunitySection.CardSubTitle')}</p>
